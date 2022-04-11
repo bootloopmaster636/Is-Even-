6912446644b4b286f056260666ed47530c702f5e
bootloopmaster636 <christhartono@hotmail.com>
2022-04-11 08:09:24 +0700

    Add odd and menu

--- main.c
+++ main.c
@@ -4,11 +4,36 @@
 
 int main() {
     int num;
+    char menu;
     bool res;
 
-    printf("Enter a number: "); scanf("%d", &num);
+    do {
+        system("cls");
+        printf("Want is odd or is even? 0dd / 3ven? "); menu = getch();
 
-    res = (num % 2 == 0);
+        switch (menu) {
+        case '0':
+            printf("Enter a number: "); scanf("%d", &num);
+            res = (num % 2 != 0);
+            printf("Is %d odd? %d (1 yes, 0 not)", num, res);
+            break;
 
-    printf("Is %d even? %d (1 yes, 0 not)", num, res);
+        case '3':
+            printf("Enter a number: "); scanf("%d", &num);
+            res = (num % 2 == 0);
+            printf("Is %d even? %d (1 yes, 0 not)", num, res);
+            break;
+
+        case 27:
+            printf("See you next time");
+            break;
+
+        default:
+            break;
+        } if (menu != 27) getch();
+
+    } while (menu != 27);
+
+    getch();
+    return 0;
 }
