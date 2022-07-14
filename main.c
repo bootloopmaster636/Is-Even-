67d30a1ed5f0bdf4d78bd51da6341e0db050d1ea
Christopher H <christhartono@hotmail.com>
2022-07-14 09:11:43 +0700

    Compatibility on linux

--- main.c
+++ main.c
@@ -1,39 +1,47 @@
+#include <curses.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+
 
 int main() {
-    int num;
-    char menu;
-    bool res;
+  int num;
+  char menu;
+  bool res;
 
-    do {
-        system("cls");
-        printf("Want is odd or is even? 0dd / 3ven? "); menu = getch();
+  do {
+    // greetings!
+    system("cls");
+    printf("Want is odd or is even? 0dd / 3ven? ");
+    menu = getch();
 
-        switch (menu) {
-        case '0':
-            printf("Enter a number: "); scanf("%d", &num);
-            res = (num % 2 != 0);
-            printf("Is %d odd? %d (1 yes, 0 not)", num, res);
-            break;
+    switch (menu) {
+    case '0':
+      printf("Enter a number: ");
+      scanf("%d", &num);
+      res = (num % 2 != 0);
+      printf("Is %d odd? %d (1 yes, 0 not)", num, res);
+      break;
 
-        case '3':
-            printf("Enter a number: "); scanf("%d", &num);
-            res = (num % 2 == 0);
-            printf("Is %d even? %d (1 yes, 0 not)", num, res);
-            break;
+    case '3':
+      printf("Enter a number: ");
+      scanf("%d", &num);
+      res = (num % 2 == 0);
+      printf("Is %d even? %d (1 yes, 0 not)", num, res);
+      break;
 
-        case 27:
-            printf("See you next time");
-            break;
+    case 27:
+      printf("See you next time");
+      break;
 
-        default:
-            break;
-        } if (menu != 27) getch();
+    default:
+      break;
+    }
+    if (menu != 27)
+      getch();
 
-    } while (menu != 27);
+  } while (menu != 27);
 
-    getch();
-    return 0;
+  getch();
+  return 0;
 }
